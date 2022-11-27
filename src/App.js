080c8b385c2e0b0bcc5b0a8056739c2d117f2055
Grebenkina <grebenkina.2490@gmail.com>
2022-11-27 18:58:12 +0800

    gh pages added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { Recipe } from './pages/Recipe';
 function App() {
   return (
     <>
-  <Router>
+  <Router basename='/food-project'>
     <Header />
       <main className="container content">
           <Switch>
